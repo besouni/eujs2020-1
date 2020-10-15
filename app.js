352985daa17dc34ace791dccf54d2d8457cc0e95
besouni <55584455+besouni@users.noreply.github.com>
2020-10-15 16:15:53 +0400

    If

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ function lab3_1(){
     x="JavaScript is the world's most pOpULar programming language.";
     document.write(x.indexOf("popular"));
     document.write("<br>");
-    document.write(x.match(new RegExp("popular", "gi")));
+    if(x.match(new RegExp("popular", "gi"))!=null){
+        document.write("Yes");
+    }else{
+        document.write("No");
+    }                   
 }
